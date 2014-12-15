40d474483e2d2f0a429f1b2f7138d6323b9423d3
Fechin <lihuoqingfly@163.com>
2014-12-15 15:37:38 +0800

    Add column dislocation repair script

---
+++ colrepair/README.md
@@ -0,0 +1,56 @@
+### 使用说明
+------
+
+
+*该脚本用于修复列错位的数据，需提供`xls`修复文档，格式如下，将列值等于`oldContentID`更新为`contentID`*
+
+contentID     | oldContentID
+--------------| -------------
+SER_Bill_Ep19 |
+SER_Bill_Ep20 |
+SER_Bill_Ep22 | SER_Bill_Ep21
+SER_Bill_Ep24 | SER_Bill_Ep22
+SER_Bill_Ep25 | SER_Bill_Ep23
+SER_Bill_Ep27 | SER_Bill_Ep23
+
+### 安装依赖
+----
+* PyYAML  # 配置格式
+* xlrd    # xls读取
+* MySQL-python
+
+```
+pip install -r requirements.txt
+```
+
+### 配置说明
+------
+``` yaml
+# 查询服务器
+select-server:
+    host: "192.168.20.130"
+    port: "3306"
+    db  : "mnj_heartbeat"
+    user: "orbit_java"
+    pawd: "orbit_java"
+
+# 更新服务器
+update-server:
+    host: "192.168.20.130"
+    port: "3306"
+    db  : "ivmall"
+    user: "orbit_java"
+    pawd: "orbit_java"
+
+# 待处理xls数据
+xls-name: "file.xls"
+# 正确列的rownum
+xls-col-right: 0
+# 错误列的rownum
+xls-col-error: 1
+
+# 表名
+table-name : "question"
+# 字段名
+column-name: "contentId"
+```
---
+++ colrepair/repair.py
@@ -0,0 +1,94 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2014 Fechin <lihuoqingfly@163.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+列错位修复脚本
+"""
+
+from yaml import load
+import sys,time, MySQLdb, xlrd
+
+def load_config( filepath = '/conf.yaml' ):
+    '''
+    加载配置文件
+    '''
+    f = open(sys.path[0] + filepath)
+    result = load(f)
+    f.close()
+    return result
+
+def read_xls( filepath = 'file.xls',by_index = 0 ):
+    '''
+    读取xls文档, 返回第by_index个Sheet
+    '''
+    book = xlrd.open_workbook( filepath )
+    table = book.sheet_by_index( by_index )
+    return table
+
+def select(sv,sql):
+    '''
+    查询数据
+    '''
+    try:
+        db  = MySQLdb.connect(host=sv['host'], user=sv['user'],\
+                passwd=sv['pawd'], db=sv['db'])
+        cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        cur.execute(sql)
+        rs = cur.fetchall()
+        cur.close()
+        return rs
+    except Exception, e:
+        raise e
+
+def update(sv,sql):
+    '''
+    更新数据
+    '''
+    try:
+        db  = MySQLdb.connect(host=sv['host'], user=sv['user'],\
+                passwd=sv['pawd'], db=sv['db'])
+        cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+        n = cur.execute(sql)    
+        cur.close()
+        db.commit()
+        return n > 0
+    except Exception, e:
+        raise e
+
+
+def log(str):
+    '''
+    打印日志
+    '''
+    print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\t" + str
+
+def main():
+    config        = load_config()
+    select_server = config['select-server']
+    update_server = config['update-server']
+    table_name    = config['table-name']
+    column_name   = config['column-name']
+
+    table    = read_xls()
+    colright = table.col_values( config['xls-col-right'] )
+    colerror = table.col_values( config['xls-col-error'] )
+
+    for i in range(1, table.nrows):
+        if colerror[i]:
+            sql = 'SELECT id FROM %s.%s WHERE %s = "%s"' \
+                    %(select_server['db'], table_name, column_name, colerror[i])
+            data = select(select_server, sql)
+            if data:
+                ids = tuple(str(x) for y in data for x in y.values())
+                sql = 'UPDATE %s.%s SET %s = "%s" WHERE id in %s' \
+                    %(update_server['db'], table_name, column_name, colright[i], ids)
+                log("%s:%s:%s" %(i, colerror[i] , sql))
+                update(update_server, sql)
+
+if __name__ == '__main__':
+    main()
